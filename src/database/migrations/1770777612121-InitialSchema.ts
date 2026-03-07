87450c83f1e18097f5ada22c7ab7ee9c95075714
caiquangtung <caitung8@gmail.com>
2026-03-07 22:52:39 +0700

    Remove init.sql and update docker-compose.yml to eliminate the initialization script. Add migration scripts for creating database extensions and seeding initial data for users, events, and ticket types. Enhance README.md with instructions for running migrations and resetting the database.

--- src/database/migrations/1770777612121-InitialSchema.ts
+++ src/database/migrations/1770777612121-InitialSchema.ts
@@ -4,6 +4,8 @@ export class InitialSchema1770777612121 implements MigrationInterface {
     name = 'InitialSchema1770777612121'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "pgcrypto"`);
         await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('admin', 'user')`);
         await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, "email" character varying NOT NULL, "password_hash" character varying NOT NULL, "full_name" character varying NOT NULL, "profileData" jsonb NOT NULL DEFAULT '{}', "role" "public"."users_role_enum" NOT NULL DEFAULT 'user', CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE INDEX "idx_users_email" ON "users" ("email") `);
